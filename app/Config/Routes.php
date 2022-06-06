1af995d1f8c8e0446405ea183263ef372685096e
paulocoppe10 <79857716+paulocoppe10@users.noreply.github.com>
2022-06-06 15:26:14 -0300

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,13 +33,11 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::page');
 $routes->match(['get','post'], 'cadastro', 'Home::cadastro');
-$routes->match(['get','post'], 'logar', 'Home::logar');
-$routes->match(['get','post'], 'login', 'Home::login');
 $routes->match(['get','post'], 'excluir/(:num)', 'Home::excluir/$1');
 $routes->match(['get','post'], 'editar/(:num)', 'Home::editar/$1');
 $routes->match(['get','post'], 'gravar', 'Home::gravar');
 $routes->match(['get','post'], 'editar/gravar', 'Home::gravar');
-$routes->get('pessoa', 'Home::pessoas');
+$routes->get('veiculo', 'Home::veiculo');
 $routes->get('(:any)', 'Home::page/$1');
 
 /*
